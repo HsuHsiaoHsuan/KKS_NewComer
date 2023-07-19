e6303c4a438e71a7634a97ebafe67a56ed482f2e
Freeman Hsu <hsiaohsuanhsu@kkstream.com>
2023-07-19 16:42:42 +0800

    use navigation component & safe args pass data to open DetailFragment

--- app/src/main/java/com/example/kks_newcomer/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/kks_newcomer/ui/detail/DetailFragment.kt
@@ -5,23 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kks_newcomer.R
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.example.kks_newcomer.data.Attraction
-import timber.log.Timber
+import com.example.kks_newcomer.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val data = arguments?.getParcelable<Attraction>("attraction")
 
-        Timber.d(data.toString())
+        if (data?.images != null) {
+            binding.recyclerImages.apply {
+                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                adapter = ImageGalleryAdapter(data.images.map { it.src })
+                PagerSnapHelper().attachToRecyclerView(this)
+            }
+        } else {
+            binding.recyclerImages.isVisible = false
+        }
+        binding.textName.text = data?.name
+        binding.textAddress.text = data?.address
+        binding.textTel.text = data?.tel
+        binding.textWebsite.text = data?.officialSite
+        binding.textIntroduction.text = data?.introduction
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
